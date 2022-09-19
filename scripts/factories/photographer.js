4b3b0b501a3eb620e48f530656402e407738176b
swanndolia <swanndolia@gmail.com>
2022-09-19 18:01:19 +0200

    added all picture in repo + completed main page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,48 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, country, city, tagline, price, portrait, id} = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const photographerLink = ["photographer.html", id].join("?id=")
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const article = document.createElement('article');
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        const figureLink = document.createElement('a');
+        const nameElem = document.createElement('h2');
+        const figcaption = document.createElement('figcaption');
+        const locationElem = document.createElement('p'), taglineElem = document.createElement('p'), priceElem = document.createElement('p');
+        figureLink.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(nameElem);
+        article.appendChild(figureLink);
+        figcaption.appendChild(locationElem);
+        figcaption.appendChild(taglineElem);
+        figcaption.appendChild(priceElem);
+        article.appendChild(figcaption);
+        nameElem.textContent = name;
+        locationElem.textContent = [city, country].join(", ")
+        taglineElem.textContent = tagline;
+        priceElem.textContent = price;
+        addCSSToDOM(article, figure, figureLink, figcaption, nameElem, locationElem, taglineElem, priceElem, img);
+        return article;
     }
+    function addCSSToDOM(article, figure, figureLink, figcaption, nameElem, locationElem, taglineElem, priceElem, img) {
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
+        figureLink.setAttribute('href', photographerLink)
+        img.style.borderRadius = "50%";
+        article.style.display = 'flex'
+        article.style.flexDirection = 'column'
+        article.style.alignItems = 'center'
+        article.style.justifyContent = 'center'
+        figure.style.margin = '0'
+        figcaption.style.lineHeight = "40%"
+        locationElem.style.color = "#901C1C"
+        priceElem.style.color = "grey"
+        figure.childNodes.forEach(element => element.style.margin = "0")
+        article.childNodes.forEach(element => element.style.textAlign = "center")
+    }
+
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,21 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    const response = await fetch("data/photographers.json")
+    return await response.json()
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+    photographers.forEach((media) => {
+        const photographerModel = photographerFactory(media);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
